02df0cedba9bda9465f773415033b79842429965
itsmejamai <annjamaicagaupo@gmail.com>
2025-10-08 21:41:26 +0800

    Migrations

---
+++ database/migrations/2025_10_08_100127_create_routes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('routes', function (Blueprint $table) {
+            $table->id();
+            $table->string('start_point');
+            $table->string('end_point');
+            $table->text('shortest_path');
+            $table->decimal('distance', 8, 2);
+            $table->string('travel_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('routes');
+    }
+};
---
+++ database/migrations/2025_10_08_100130_create_deliveries_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('route_id')->constrained()->onDelete('cascade');
+            $table->string('pickup_location');
+            $table->string('dropoff_location');
+            $table->enum('delivery_status', ['pending', 'in-transit', 'delivered'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deliveries');
+    }
+};
